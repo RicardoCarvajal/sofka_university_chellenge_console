c8ce8ed4d0283a3242c33cb60b32fb1364cccde3
Ricardo Carvajal <rcarvajal@intelix.biz>
2023-11-02 11:32:32 -0400

    Creando proyecto de practica para softka

---
+++ sofka_challenge_console/src/main/java/com/intelix/challenge/app/SofkaChallengeConsoleApplication.java
@@ -0,0 +1,13 @@
+package com.intelix.challenge.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SofkaChallengeConsoleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SofkaChallengeConsoleApplication.class, args);
+	}
+
+}
