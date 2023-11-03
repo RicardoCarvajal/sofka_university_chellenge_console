eba62ecb24a428445613796752b0ae6158b10997
Ricardo Carvajal <rcarvajal@intelix.biz>
2023-11-03 17:17:32 -0400

    feat(configuracion): adding parameters                      of properties ----------------------------------------- Detailed description:
    BREAKING CHANGE: new is added
    configuration from the property of the
    application to choose from the property
    what will it generate, if it generates the prices
    totals or if you generate the products with
    the requested characteristics
    -----------------------------------------

--- sofka_challenge_console/src/main/java/com/intelix/challenge/app/SofkaChallengeConsoleApplication.java
+++ sofka_challenge_console/src/main/java/com/intelix/challenge/app/SofkaChallengeConsoleApplication.java
@@ -1,14 +1,24 @@
 package com.intelix.challenge.app;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import com.intelix.challenge.app.conf.PropertiesConf;
 
 import lombok.extern.log4j.Log4j2;
 
 @Log4j2
 @SpringBootApplication
 public class SofkaChallengeConsoleApplication implements CommandLineRunner {
+
+	PropertiesConf propertiesConf;
+
+	@Autowired
+	public SofkaChallengeConsoleApplication(PropertiesConf propertiesConf) {
+		this.propertiesConf = propertiesConf;
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(SofkaChallengeConsoleApplication.class, args);
@@ -17,6 +27,16 @@ public class SofkaChallengeConsoleApplication implements CommandLineRunner {
 	@Override
 	public void run(String... args) throws Exception {
 		log.info("Iniciando aplicacion");
+
+		log.info("Opcion: " + propertiesConf.getType());
+
+		if (propertiesConf.getType() == 1) {
+
+		} else {
+
+		}
+
+		log.info("Finalizando el proceso");
 
 	}
 
---
+++ sofka_challenge_console/src/main/java/com/intelix/challenge/app/conf/PropertiesConf.java
@@ -0,0 +1,15 @@
+package com.intelix.challenge.app.conf;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+
+import lombok.Data;
+
+@Data
+@Configuration
+@ConfigurationProperties(prefix = "consoleapp.option")
+public class PropertiesConf {
+
+	private int type;
+
+}
