4841f0f29167f128b4f2e7ff6335a38359706367
Ricardo Carvajal <rcarvajal@intelix.biz>
2023-11-03 12:48:09 -0400

    feat(structure): The project structure is started ----------------------------------------- Detailed description:
    BREAKING CHANGE: Necessary change for
    project development and facilitate the
    safety and good practices
    -----------------------------------------

--- sofka_challenge_console/src/main/java/com/intelix/challenge/app/SofkaChallengeConsoleApplication.java
+++ sofka_challenge_console/src/main/java/com/intelix/challenge/app/SofkaChallengeConsoleApplication.java
@@ -1,13 +1,22 @@
 package com.intelix.challenge.app;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import lombok.extern.log4j.Log4j2;
+
+@Log4j2
 @SpringBootApplication
-public class SofkaChallengeConsoleApplication {
+public class SofkaChallengeConsoleApplication implements CommandLineRunner {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SofkaChallengeConsoleApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		log.info("Iniciando procesos de creación de data");
 	}
 
 }
